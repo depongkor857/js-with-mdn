5df9cb21e19d55d3a929a70f8045fd7313dca1d1
depongkor857 <dev.depongkor@gmail.com>
2021-12-22 11:50:30 +0600

    Looping through an array with filter method

--- loop/loop.js
+++ loop/loop.js
@@ -1,9 +1,25 @@
-function toUpper(string){
-    return string.toUpperCase();
+// let cats = ['leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+
+// for (let cat of cats) {
+//     console.log(cat);
+// }
+
+// function toUpper(string){
+//     return string.toUpperCase();
+// }
+
+// const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+
+// const upperCats = cats.map(toUpper);
+
+// console.log(upperCats);
+
+function lcat(cat){
+    return cat.startsWith("L");
 }
 
 const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
-const upperCats = cats.map(toUpper);
+const filtered = cats.filter(lcat);
 
-console.log(upperCats);
+console.log(filtered);
